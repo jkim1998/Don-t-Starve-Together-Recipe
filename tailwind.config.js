98d5428c7f2f31029bf06bc7ae50b683199a7543
Jonathan Kim <jonathankim980@gmail.com>
2023-02-22 00:26:37 -0600

    reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    fontFamily: { DST: "DST" },
-    // colors: {
-    //   cookbook: "#ffae68",
-    // },
     extend: {
       backgroundImage: {
-        frame: "url('/src/data/image/frame.png')",
+        frame: "url('/src/data/frame.png')",
       },
     },
   },
